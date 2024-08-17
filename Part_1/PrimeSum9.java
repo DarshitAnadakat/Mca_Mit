b064351fe4f79823b29ed21174df931c9709f16e
DarshitAnadakat <110161814+DarshitAnadakat@users.noreply.github.com>
2024-08-17 17:07:46 +0530

    Update PrimeSum9.java

--- Part_1/PrimeSum9.java
+++ Part_1/PrimeSum9.java
@@ -1,3 +1,15 @@
+/*9. Write a Java program to compute the sum of the first 100 prime numbers
+10. Write a Java program to swap the first and last elements of an array and create a
+new array
+11. Write a Java program to count the number of even and odd elements in a given
+array
+12. Write a Java program to check if a positive number is a palindrome or not
+13. Write a Java program to add two numbers without using any arithmetic
+operators
+14. Write a Java program to add all the digits of a given positive integer
+15. Write a java program to Compute the Sum of the Principal and Secondary
+Diagonals elements of a Matrix*/
+
 public class PrimeSum9 {
     public static void main(String[] args) {
         int count = 0;
